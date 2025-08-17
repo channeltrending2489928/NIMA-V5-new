22db1c3204a8653711ba39e9381d10b8e1b4bf83
channeltrending2489928 <channeltrending2489928@gmail.com>
2025-08-17 18:41:40 +0530

    Update settings.js

--- setting/settings.js
+++ setting/settings.js
@@ -1,16 +1,16 @@
 const fs = require('fs')
 
 //~~~~~~~~~~~ Settings Owner ~~~~~~~~~~~//
-global.owner = "94726800969"
-global.developer = "94726800969"
+global.owner = "94741035694"
+global.developer = "94741035694"
 global.bot = ""
-global.devname = "NIMESHA"
-global.ownername = "NIMESHA"
-global.botname = "NIMA-V5"
+global.devname = "♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡ "
+global.ownername = "♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡ "
+global.botname = "♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡"
 global.versisc = "2"
-global.packname = "⎋NIMA-V5"
+global.packname = "♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡"
 //~~~~~~~~~~~ Settings Sosmed ~~~~~~~~~~~//
-global.linkwa = "https://wa.me/94726800969"
+global.linkwa = "https://wa.me/94741035694"
 global.linkyt = "https://youtube.com/@nimaedition?si=k-p-6UVUdgz2jsPb"
 global.linktt = "https://tiktok.com"
 global.linktele = "https://t.me"
@@ -32,7 +32,7 @@ global.thumbown = "https://files.catbox.moe/w9lv7j.jpg"
 
 //~~~~~~~~~~~ Settings Channel ~~~~~~~~~~~//
 global.idchannel = "120363419075720962@newsletter*"
-global.channelname = "ーNIMA-V5 UPDATES"
+global.channelname = "-♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡ "
 global.channel = "hatsapp.com/channel/0029Vb68g1c3LdQLQDkbAQ3M"
 
 //~~~~~~~~~~~ Settings Message ~~~~~~~~~~~//
@@ -42,7 +42,7 @@ global.mess = {
   group: " `[ සමූහයක පමණි!! ]` \n මේ feature එක සමූහයක පමණි!!",
   private: " `[ පෞද්ගලිකව පමණි!! ]` \n මේ feature එක පෞද්ගලිකව පමණි!!",
   admin: " `[ ඇඩ්මින් හට පමණි!! ]` \n මේ feature එක ඇඩ්මින් හට පමණි!!",
-  botadmin: " `[ නිමේෂ හට පමණි!! ]` \n මේ feature එක නිමේෂ හට පමණි!!",
+  botadmin: " `[ ♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡  හට පමණි!! ]` \n මේ feature එක ♡ 𝐒𝐮𝐧𝐧𝐲 𝐓𝐞𝐜𝐡 𝐁𝐫𝐨 ♡  හට පමණි!!",
   wait: " `[ රැඳීසිටින්න!! ]` \n රැඳීසිටින්න, loading...",
   error: " `[ අසාර්ථකයි!! ]` \n එය අසාර්ථක උණි!!",
   done: " `[ සාර්ථකයි!! ]` \n සේවාව සාර්ථකයි!!"
